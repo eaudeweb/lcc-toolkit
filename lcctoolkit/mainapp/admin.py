4549b90a8155ce2015ce94678fd898b11b83548f
gabrielastrezea <strezea_gabriela@yahoo.com>
2017-09-12 11:08:24 +0300

    Add models for Taxonomy Tags and Clasifications. Add fixtures for all models.

--- lcctoolkit/mainapp/admin.py
+++ lcctoolkit/mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from lcctoolkit.mainapp import models
+
 
 # Register your models here.
+admin.site.register(models.TaxonomyTagGroup)
+admin.site.register(models.TaxonomyTag)
+admin.site.register(models.TaxonomyClassification)
