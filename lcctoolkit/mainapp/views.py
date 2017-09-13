4b558cbc7bbb9a1ffbc04709fee1356a71cef0e8
Catalin Ioan Curcanu <catalin.curcanu@eaudeweb.ro>
2017-09-13 17:35:13 +0300

    Setting up login & logout views

--- lcctoolkit/mainapp/views.py
+++ lcctoolkit/mainapp/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import django.contrib.auth as auth
+import django.shortcuts
+import django.http
+
+import json
 
 
 def index(request):
-    return render(request, "index.html", {})
+    return django.shortcuts.render(request, "index.html", {})
+
+
+def login(request):
+    if request.method == "GET":
+        return django.shortcuts.render(request, "login.html", {})
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(request, username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return django.http.HttpResponse(json.dumps({'msg': 'success'}))
+        else:
+            return django.http.HttpResponse(json.dumps({'msg': 'failure'}))
+
+def logout(request):
+        auth.logout(request)
+        return django.http.HttpResponseRedirect("/")
--- lcctoolkit/urls.py
+++ lcctoolkit/urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls import url, include
+import lcctoolkit.mainapp.views as views
+
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('lcctoolkit.mainapp.urls')),
+    url(r'^login/',views.login),
+    url(r'^logout/', views.logout),
+    url(r'^$', views.index),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
