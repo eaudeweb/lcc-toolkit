d82d1a28f3bd3308cf97dc64997dc27e1f7d963f
Catalin Ioan Curcanu <catalin.curcanu@eaudeweb.ro>
2017-09-18 16:23:05 +0300

    Merge with latest updated on develop

--- lcctoolkit/urls.py
+++ lcctoolkit/urls.py
@@ -7,10 +7,13 @@ from django.contrib import admin
 from django.conf import settings
 
 
+OTHER_URLS = (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+              static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', views.Login.as_view()),
     url(r'^logout/', views.Logout.as_view()),
     url(r'^legislation/', views.ListLaws.as_view()),
     url(r'^$', views.Index.as_view()),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + OTHER_URLS
