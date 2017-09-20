c6d305fef64662e1674dcb7c5126c8168898b186
Catalin Ioan Curcanu <7123909+ccurcanu@users.noreply.github.com>
2017-09-20 14:42:39 +0300

    Fix typo in urls

--- lcctoolkit/urls.py
+++ lcctoolkit/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     url(r'^login/', views.Login.as_view()),
     url(r'^logout/', views.Logout.as_view()),
     url(r'^legislation/add/$', views.LegislationAdd.as_view()),
-    url(r'^legislation/', views.LegislationExplorere.as_view(), name="legislation"),
+    url(r'^legislation/', views.LegislationExplorer.as_view(), name="legislation"),
     url(r'^$', views.Index.as_view()),
 ] + OTHER_URLS
