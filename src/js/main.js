c85f076845e0860e5e4672046bf4f87e46824899
mihai-macaneata <themacaneata@gmail.com>
2017-09-25 15:38:34 +0300

    styling for first 3 fields in /add

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,22 @@
-$(document).ready(function (){
-	$('.list-header').on('click',function(e){
-		if($(e.target).attr('type') == "checkbox"){
-			return
-		}
+$(document).ready(function() {
+    $('.list-header').on('click', function(e) {
+        if ($(e.target).attr('type') == "checkbox") {
+            return
+        }
 
-		$checkbox = $(this).find('i').first();
-		$sub_level = $(this).parent().find('ul').first();
-		$sub_level.animate({
-			'height':'toggle'
-		})
-		$checkbox.toggleClass('fa-minus-square')
-	})
+        $checkbox = $(this).find('i').first();
+        $sub_level = $(this).parent().find('ul').first();
+        $sub_level.animate({
+            'height': 'toggle'
+        })
+        $checkbox.toggleClass('fa-minus-square')
+    })
+
+ 	$("#rem_char").html("1024 characters left");
+
+    $('textarea').keyup(function() {
+        $("#rem_char").html((1024 - this.value.length) + " characters left");
+    })
 })
+
+
