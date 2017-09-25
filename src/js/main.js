06be5eb701d1317eefadaaf4ae5f1fd3913527a0
mihai-macaneata <themacaneata@gmail.com>
2017-09-25 18:19:30 +0300

    tags and classifications check functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,31 @@
+function highlightChecked(item) {
+    $parent = item.parent().parent().parent().parent('li').find('.list-header').first();
+    $parent.addClass('selected')
+    console.log($parent)
+    if (!$parent.parent().hasClass('first-level')) {
+        $recursion_item = $parent.find('input')
+        highlightChecked($recursion_item)
+    }
+}
+
+function removeHighlight(item) {
+    $parent = item.parent().parent().parent().parent('li').find('.list-header').first();
+    $checked = item.closest('ul').first().find("input:checked").length;
+    if ($checked == 0)
+        $parent.removeClass('selected')
+    if (!$parent.parent().hasClass('first-level')) {
+        $recursion_item = $parent.find('input')
+        removeHighlight($recursion_item)
+    }
+}
+
+
+
 $(document).ready(function() {
     $('.list-header').on('click', function(e) {
         if ($(e.target).attr('type') == "checkbox") {
             return
         }
-
         $checkbox = $(this).find('i').first();
         $sub_level = $(this).parent().find('ul').first();
         $sub_level.animate({
@@ -12,11 +34,18 @@ $(document).ready(function() {
         $checkbox.toggleClass('fa-minus-square')
     })
 
- 	$("#rem_char").html("1024 characters left");
+    $("#rem_char").html("1024 characters left");
 
     $('textarea').keyup(function() {
         $("#rem_char").html((1024 - this.value.length) + " characters left");
     })
+
+
+    $(".check-fields .second-level input").change(function() {
+        if (this.checked) {
+            highlightChecked($(this))
+        } else {
+            removeHighlight($(this))
+        }
+    });
 })
-
-
