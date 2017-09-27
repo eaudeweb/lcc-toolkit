335cb5081b5a5625565948ad6c613f9e4a3ff24f
Catalin Ioan Curcanu <catalin.curcanu@eaudeweb.ro>
2017-09-27 13:36:38 +0300

    Making postgres as default backend database

--- lcctoolkit/mainapp/views.py
+++ lcctoolkit/mainapp/views.py
@@ -2,6 +2,7 @@ import json
 import os
 import pdftotext
 
+import django.db
 import django.contrib.auth as auth
 import django.shortcuts
 import django.http
@@ -150,16 +151,11 @@ class LegislationAdd(django.views.View):
                     pk__in=selected_ids)
 
         def add_legislation_page(law):
-            with open(os.path.join(settings.MEDIA_ROOT, law.pdf_file_name), "rb") as f:
-                pdf = pdftotext.PDF(f)
-
-            counter = 1
-            for page in pdf:
-                models.LegislationPage.objects.create(
-                    page_text="<pre>%s</pre>" % page,
-                    page_number=counter,
-                    legislation=law)
-                counter = counter + 1
+            with open(os.path.join(settings.MEDIA_ROOT, law.pdf_file.name), "rb") as fd:
+                pdf = pdftotext.PDF(fd)
+            with django.db.transaction.atomic():
+                for idx, page in enumerate(pdf):
+                    models.LegislationPage(page_text="<pre>%s</pre>" % page, page_number=idx+1, legislation=law).save()
 
         law_obj = models.Legislation()
         law_obj.law_type = request.POST["law_type"]
