30b5ea13cc9b1941ce579fb158b8c3ec2771b099
stefanvasilica <themacaneata@gmail.com>
2017-09-28 13:36:03 +0300

    checkboxes styling fix

--- lcctoolkit/settings.py
+++ lcctoolkit/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'lcctoolkit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lcct',
-        'USER': 'postgres',
-        'PASSWORD': 'Admin123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'lcct',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Admin123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
