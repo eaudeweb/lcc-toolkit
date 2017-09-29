05989e33d602c18f595fc56b27e02f48f9f09525
stefanvasilica <themacaneata@gmail.com>
2017-09-29 14:14:48 +0300

    pdf sync with ajax pages

--- lcctoolkit/mainapp/static/js/add_articles.js
+++ lcctoolkit/mainapp/static/js/add_articles.js
@@ -1,3 +1,8 @@
+function changePage(item, page_number) {
+    var pdf_page = item.attr('data').replace(/#page=\d+/, '#page='+page_number)
+    item.attr("data", pdf_page);
+}
+
 $(document).ready(function(){
 
   var AjaxSubmit = {};
@@ -24,8 +29,9 @@ $(document).ready(function(){
     document.getElementById("page_number").innerHTML = page_number;
     document.getElementById("id_page").value = page_number;
     // TODO verify this
+    changePage($('.pdf'), page_number)
     $("#next").prop("disabled", false);
-    if( $page_number == 0) 
+    if( page_number == 1)
       $("#prev").prop("disabled", true);
     else
       $("#prev").prop("disabled", false);
@@ -36,8 +42,9 @@ $(document).ready(function(){
     document.getElementById("raw-text-page").innerHTML = pages[page_number];
     document.getElementById("page_number").innerHTML = page_number;
     document.getElementById("id_page").value = page_number;
+    changePage($('.pdf'), page_number)
     $("#prev").prop("disabled", false);
-    if( page_number == $max_page) 
+    if( page_number == $max_page)
       $("#next").prop("disabled", true);
     else
       $("#next").prop("disabled", false);
