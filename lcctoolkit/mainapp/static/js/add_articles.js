0ac9a665ff167c43cf5bec953d45c507695e60b6
gabrielastrezea <strezea_gabriela@yahoo.com>
2017-10-02 16:43:59 +0300

    Add 'Save & View articles' button on 'Add articles' page

--- lcctoolkit/mainapp/static/js/add_articles.js
+++ lcctoolkit/mainapp/static/js/add_articles.js
@@ -6,7 +6,7 @@ function changePage(item, page_number) {
 $(document).ready(function(){
 
   var AjaxSubmit = {};
-  AjaxSubmit["law_id"] = $("#law_pk").val();
+  AjaxSubmit["law_id"] = parseInt($("#law_pk").val());
   var page_number = parseInt($("#page_number").text());
   var pages = null;
 
@@ -20,15 +20,25 @@ $(document).ready(function(){
     }
   });
 
+  var input_law_id = $("<input>")
+                 .attr("type", "hidden")
+                 .attr("name", "law_id").val(AjaxSubmit["law_id"]);
+  var input_page = $("<input>")
+                 .attr("type", "hidden")
+                 .attr("id", "form_page_id")
+                 .attr("name", "page").val(page_number);
+  $('#addArticle').append($(input_law_id));
+  $('#addArticle').append($(input_page));
+
   $max_page = Object.keys(pages).length;
   document.getElementById("raw-text-page").innerHTML = pages[$("#starting_page").val()];
 
   $("#prev").on('click', function(){
     page_number = page_number - 1;
-    document.getElementById("raw-text-page").innerHTML = pages[page_number];
-    document.getElementById("page_number").innerHTML = page_number;
-    document.getElementById("id_page").value = page_number;
-    // TODO verify this
+    $("#raw-text-page").html(pages[page_number]);
+    $("#page_number").html(page_number);
+    $("#id_page").val(page_number);
+    $("#form_page_id").val(page_number);
     changePage($('.pdf'), page_number)
     $("#next").prop("disabled", false);
     if( page_number == 1)
@@ -39,9 +49,10 @@ $(document).ready(function(){
 
   $("#next").on('click', function(){
     page_number = page_number + 1;
-    document.getElementById("raw-text-page").innerHTML = pages[page_number];
-    document.getElementById("page_number").innerHTML = page_number;
-    document.getElementById("id_page").value = page_number;
+    $("#raw-text-page").html(pages[page_number]);
+    $("#page_number").html(page_number);
+    $("#id_page").val(page_number);
+    $("#form_page_id").val(page_number);
     changePage($('.pdf'), page_number)
     $("#prev").prop("disabled", false);
     if( page_number == $max_page)
@@ -71,6 +82,5 @@ $(document).ready(function(){
       }
     });
   });
-
 
  });
