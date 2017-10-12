e49df4315200cf64187289d9413790a0757e13bb
gabrielastrezea <strezea_gabriela@yahoo.com>
2017-10-12 11:16:43 +0300

    Add models for Assessment review

--- lcctoolkit/mainapp/admin.py
+++ lcctoolkit/mainapp/admin.py
@@ -12,3 +12,7 @@ admin.site.register(models.Country)
 admin.site.register(models.TaxonomyTagGroup)
 admin.site.register(models.TaxonomyTag)
 admin.site.register(models.TaxonomyClassification)
+
+admin.site.register(models.Question)
+admin.site.register(models.Assessment)
+admin.site.register(models.Answer)
