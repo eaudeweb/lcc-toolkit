fbacf804012e188d8e1e58dacc680a6fcbe4fa9a
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2017-10-13 12:40:32 +0300

    integrate sentry user feedback

--- lcctoolkit/urls.py
+++ lcctoolkit/urls.py
@@ -1,3 +1,4 @@
+import lcctoolkit.views as project_views
 import lcctoolkit.mainapp.views as views
 
 from django.conf.urls import url
@@ -10,12 +11,14 @@ from django.conf import settings
 OTHER_URLS = (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
+handler500 = 'lcctoolkit.views.handler500'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', views.Login.as_view()),
     url(r'^logout/', views.Logout.as_view()),
 
-    url(r'^crashme$', views.crashme, name='crashme'),
+    url(r'^crashme$', project_views.crashme, name='crashme'),
 
     url(r'^legislation/add/$', views.LegislationAdd.as_view()),
     url(r'^legislation/edit/.*$', views.LegislationEditView.as_view()),
