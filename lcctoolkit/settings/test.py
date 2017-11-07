5686547dbb04101324534343167c24f3ab7d983c
Ariel Pontes <pontes.ariel@gmail.com>
2017-11-07 11:18:52 +0200

    Add postgres to travis setup.

--- lcctoolkit/settings/test.py
+++ lcctoolkit/settings/test.py
@@ -1,8 +1,20 @@
+import os
 import environ
 from lcctoolkit.settings.dev import *  # noqa
 
 env = environ.Env(DEBUG=(bool, False),)
 
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'HOST': 'localhost',
+            'NAME': 'test_lcc',
+            'PASSWORD': '',
+            'PORT': 5432,
+            'USER': 'postgres'
+        }
+    }
 
 ELASTICSEARCH_DSL = {
     'default': {
