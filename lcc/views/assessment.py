2942f141f620b3793c037b8c2c2febe54678d3c6
gabrielastrezea <strezea_gabriela@yahoo.com>
2017-12-11 11:37:17 +0200

    Add raw template for results pdf

--- lcc/views/assessment.py
+++ lcc/views/assessment.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import mixins
 from django.views.generic import TemplateView
 from lcc.views.api import AssessmentResults
-# from rest_framework.response import Response
+from rest_framework.response import Response
 from django.http import HttpResponse
 from rest_framework.renderers import TemplateHTMLRenderer
 from lcc import serializers, utils
@@ -21,10 +21,26 @@ class LegalAssessmentResultsPDF(AssessmentResults):
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        result = serializers.AssessmentResultSerializer(self.object)
-        pdf = utils.render_to_pdf('results_pdf.html', result.data)
+        results = serializers.AssessmentResultSerializer(self.object)
+
+        top_categories = len(results.data['categories'])
+        areas = 0
+        suggestions = 0
+        for category in results.data['categories']:
+            for subcategory in category['categories']:
+                areas += len(subcategory['questions'])
+                for question in subcategory['questions']:
+                    suggestions += len(question['articles'])
+
+        context = {
+            'results': results.data,
+            'host': request.META['HTTP_HOST'],
+            'categories': top_categories,
+            'areas': areas,
+            'law_suggestions': suggestions
+        }
+
+        # For html version, comment lines 44-45 and uncomment line 46
+        pdf = utils.render_to_pdf('results_pdf.html', context)
         return HttpResponse(pdf, content_type='application/pdf')
-        # return Response(
-        #     {'result': result.data},
-        #     template_name='results_pdf.html'
-        # )
+        # return Response(context, template_name='results_pdf.html')
