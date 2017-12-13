7320bfc0cae99fa69636ba617c47aaf600f6e144
mihai.macaneata <mihai.macaneata@eaudeweb.ro>
2017-12-13 11:57:47 +0200

    moved scripts from cdn locally

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,8 @@ module.exports = function(grunt) {
                 files: {
                     "lcc/static/css/main.css": "lcc/src/less/main.less",
                     "lcc/static/css/jquery-ui.css": "lcc/src/less/lib/jquery-ui.less",
-                    "lcc/static/css/download_results.css": "lcc/src/less/lib/download_results.less"
+                    "lcc/static/css/download_results.css": "lcc/src/less/lib/download_results.less",
+                    "lcc/static/css/easy-autocomplete.min.css": "lcc/src/less/lib/easy-autocomplete.min.less"
                 }
             }
         },
