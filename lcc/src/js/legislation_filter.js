cdbf267a2e3d5c6d67b1d3d43d8d73deb6ae00bf
Alexandru Medesan <alexandrumedesan@Alexandrus-MacBook-Pro.local>
2018-12-05 12:59:00 +0200

    added Order By options that work query param

--- lcc/src/js/legislation_filter.js
+++ lcc/src/js/legislation_filter.js
@@ -1,10 +1,101 @@
 var payload = {};
+var orderByOptions = {
+    relevance: 'Relevance',
+    promulgation_sort: {
+        '1': 'Promulgation asc',
+        '-1': 'Promulgation desc'
+    },
+    country_sort: {
+        '1': 'Country asc',
+        '-1': 'Country desc'
+    }
+}
+var allQueryParams = '/([^?=&]+)(=([^&]*))?/g';
+// var matchOneQueryParam = `\A?${queryName}=[^&]*`;
 
+// set the page and consider the current order
 function activatePagination() {
+    let selectedOrder = null;
+    let selectedOrderValue = null;
+    let found = false;
+
     $("ul.pagination li a").on('click', function() {
+        Object.keys(orderByOptions).map((orderKey) => {
+            const queryMatch = window.location.search.match(`\A?${orderKey}=[^&]*`);
+            selectedOrder = !found && !!queryMatch ? orderKey : selectedOrder;
+            selectedOrderValue = !found && !!queryMatch ? queryMatch[0].split('=')[1] : selectedOrderValue;
+            found = !! selectedOrder;
+        });
+        !!selectedOrder ? payload[selectedOrder] = selectedOrderValue : null;
+
         payload['page'] = $(this).attr('data-page');
-        $(".submitBtn").click()
+        $(".submitBtn").click();
     });
+}
+
+function setOrderBy() {
+    let selectedOrderName = null;
+    let selectedOrderValue = null;
+    let selectedOrderKey = null;
+    let found = false;
+
+    Object.keys(orderByOptions).map((orderKey) => {
+        const queryMatch = window.location.search.match(`\A?${orderKey}=[^&]*`);
+        // if found query, change, if not keep as it was
+        selectedOrderName = !found ?
+                                queryMatch ? orderByOptions[orderKey][queryMatch[0].split('=')[1]] : ''
+                            : selectedOrderName;
+        found = !!selectedOrderName;
+
+        // if found query, change, if not keep as it was
+        selectedOrderKey = queryMatch ? orderKey : selectedOrderKey;
+
+        // if found query, change, if not keep as it was
+        selectedOrderValue = queryMatch ? queryMatch[0].split('=')[1] : selectedOrderValue;
+    });
+
+    selectedOrderName = selectedOrderName ? selectedOrderName : orderByOptions.relevance;
+    selectedOrderKey = selectedOrderKey ? selectedOrderKey : 'relevance';
+    selectedOrderValue = selectedOrderValue ? selectedOrderValue : '';
+
+    const pageQueryMatch = window.location.search.match(`\A?page=[^&]*`);
+    const pageNumber = pageQueryMatch ? pageQueryMatch[0].split('=')[1] : null;
+
+    // set the 'active' class on the order that is currently selected
+    $("ul.dropdown-menu-order li").addClass(function( index ) {
+        let className = 
+            $(this).children( "a" ).attr('data-sort-id') === selectedOrderKey &&
+            $(this).children( "a" ).attr('data-sort-dir') === selectedOrderValue ?
+                "active" : '';
+        return className;
+    });
+
+    // set the name of the preselected order
+    $("#orderBySelected").text(selectedOrderName);
+
+    // set the active class on the list of order option, to open it
+    $("#orderByBtn").on('click', function showOptions() {
+        $("#orderByParent").addClass( "active" )
+    });
+
+    // remove the 'active' class, to close the list on blur
+    $("#orderByBtn").on('blur', function showOptions() {
+        $("#orderByParent").removeClass( "active" )
+    });
+
+    // on lick set the payload and click the submit to reflect the order and the page
+    $("ul.dropdown-menu-order li a").on('click', function() {
+        const orderName = $(this).attr('data-sort-id');
+        const orderValue = $(this).attr('data-sort-dir');
+        
+        orderName !== 'relevance' ? payload[orderName] = orderValue : null;
+        pageNumber ? payload['page'] = pageNumber : null;
+        $(".submitBtn").click();
+    });
+}
+
+function findOrderQueryParam(params) {
+    
 }
 
 function preselectFilters() {
@@ -79,6 +170,7 @@ $(document).ready(function() {
     var autocomplete = [];
 
     activatePagination();
+    setOrderBy();
 
     // Activate multiselect
     $('#countrySelect').multipleSelect({
