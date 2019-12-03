3dde24007b2ba43c7cd0102c0f6d0f499b9bf219
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2019-12-03 11:27:17 +0200

    Adding script to fix fixtures - and resulting changes

--- lcc/management/commands/fix_original_questions.py
+++ lcc/management/commands/fix_original_questions.py
@@ -39,46 +39,60 @@ class Command(BaseCommand):
         )
 
     def parse_row(self, row):
-        ret = []
-        for value, level in [(row[0].value, 0), (row[3].value, 1)]:
-            if value:
-                raw_code, text = value.strip().split(' ', 1)
-                code = self.process(raw_code)
-                ret.append({
-                    'text': text,
-                    'level': level,
-                    'parent_answer': 1,
-                    'classification': code,
-                    'gap_answer': 0,
-                    'gap_classifications': [code],
-                })
-        return ret
+        # Only return data for rows that have both parent and child
+        if row[0].value is None or row[3].value is None:
+            return None, None
 
-    def create_question(self, data, parents_by_level, dry_run=False):
-        if data['level'] == 0:
-            parent = None
-        else:
-            parent = parents_by_level[data['level'] - 1]
+        parent_code, text = row[0].value.strip().split(' ', 1)
+        parent_code = self.process(parent_code)
+        
+        raw_code, text = row[3].value.strip().split(' ', 1)
+        code = self.process(raw_code)
+        ret = {
+            'text': text,
+            'level': 1,
+            'parent_answer': 1,
+            'classification': code,
+            'gap_answer': 0,
+            'gap_classifications': [code],
+        }
+        print(
+            "Found classification {} with parent {}".format(
+                code, parent_code
+            )
+        )
+        return ret, parent_code
+
+    def create_question(self, data, parent_code, dry_run=False):
+        parent_classification = TaxonomyClassification.objects.get(
+            code=parent_code
+        )
+        parent_question = Question.objects.filter(
+            classification=parent_classification,
+        ).first()
+        if not parent_question:
+            print("Parent question not found for {}".format(parent_classification))
+            return
+
         classification = TaxonomyClassification.objects.get(
             code=data['classification']
         )
         question = Question.objects.filter(
             classification=classification,
-            parent=parent
+            parent=parent_question
         ).first()
         if question:
-            parents_by_level[data['level']] = question
             print("Question for {} already created.".format(classification))
             return
         print(
             "Creating question for {} with parent {}".format(
-                classification, parent
+                classification, parent_question
              )
         )
         if not dry_run:
             question = Question.objects.create(
                 text=data['text'],
-                parent=parent,
+                parent=parent_question,
                 parent_answer=data['parent_answer'],
                 classification=classification
             )
@@ -101,24 +115,22 @@ class Command(BaseCommand):
                 gap.classifications.add(classification)
 
     def handle(self, file, *args, **options):
+        wb = load_workbook(file, read_only=True)
         self.num_prev = options.get('num_prev', 0)
         dry_run = options.get('dry_run', False)
 
-        wb = load_workbook(file, read_only=True)
-        parents_by_level = [None] * 10
         for sheet in wb:
             for row in sheet.iter_rows(min_row=2):
                 if not row or not (row[0].value or row[3].value):
                     # No relevant data in this row that cannot be inferred
                     # from other ones, skip it
                     continue
-                all_data = self.parse_row(row)
-                for data in all_data:
+                data, parent_code = self.parse_row(row)
+                if data:
                     try:
-                        question = self.create_question(data, parents_by_level, dry_run)
+                        question = self.create_question(data, parent_code, dry_run)
                         if not question:
                             continue
-                        parents_by_level[data['level']] = question
                         self.create_gap(data, question, dry_run)
                     except Exception as e:
                         print(
