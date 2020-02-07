809f084b2e16eeb25eed5dfea530b8f7485116f1
Sorin Stelian <sorin.stelian@eaudeweb.ro>
2020-02-07 16:17:13 +0200

    Also using classification from level tags

--- lcc/management/commands/import_legispro_legislation.py
+++ lcc/management/commands/import_legispro_legislation.py
@@ -112,6 +112,26 @@ class Command(BaseCommand):
                     )
                 )
 
+    def add_possible_concepts(self, possible_concepts, article_object):
+        """
+        Some concepts seem to be included in the "<level>" tag;
+        we need to check that they have the "refersto" property.
+        """
+        for concept in possible_concepts:
+            refers_to =  concept.get("refersto")
+            if not refers_to:
+                continue
+            concept_name = re.sub('[^ a-zA-z]+', '' , concept.get('title')).strip()
+            concept_code = refers_to.split("__")[1]
+            classification = find_classification(concept_name, concept_code)
+            if classification:
+                article_object.classifications.add(classification)
+                print(
+                    'Added classification {} to article.'.format(
+                        classification.code
+                    )
+                )
+
     def create_or_update_articles(self, legislation_data, legislation):
         articles = legislation_data.find_all('article')
         if not articles:
@@ -145,6 +165,9 @@ class Command(BaseCommand):
                         fields['code']
                     ))
                 self.add_concepts(article.find_all('concept'), article_object)
+                self.add_possible_concepts(
+                    article.find_all('level'), article_object
+                )
             except Exception as e:
                 if fields:
                     code = fields['code']
